ce9572ad1b1b6973769a377200ea3728b861197d
Raymond Wu <virus.warnning@gmail.com>
2016-12-27 17:41:31 +0800

    refs #17 Use official coding standard.
    * Config phpcs.xml and composer.json
    * Run autofix
    * Fix handmade problems

--- RenderTheFuck.class.php
+++ RenderTheFuck.class.php
@@ -10,108 +10,112 @@ class RenderTheFuck
 // @codingStandardsIgnoreEnd
 {
 
-    /* Version string. */
-    private static $version;
+	/* Version string. */
+	private static $version;
 
-    /* Indicate how many tags rendered. */
-    private static $rank = 0;
+	/* Indicate how many tags rendered. */
+	private static $rank = 0;
 
-    /**
-     * Setup hook tag
-     *
-     * @since 0.1.0
-     *
-     * @param $parser MediaWiki parser
-     */
-    public static function onParserFirstCallInit(&$parser)
-    {
-        // Load version string.
-        global $wgExtensionCredits;
-        foreach ($wgExtensionCredits['parserhook'] as $ext) {
-            if ($ext['name']==='RenderTheFuck') {
-                self::$version = $ext['version'];
-                break;
-            }
-        }
+	/**
+	 * Setup hook tag
+	 *
+	 * @since 0.1.0
+	 *
+	 * @param $parser MediaWiki parser
+	 */
+	public static function onParserFirstCallInit( &$parser ) {
 
-        // Setup callback for <thefuck> tag.
-        $parser->setHook('thefuck', array('RenderTheFuck', 'render'));
+		// Load version string.
+		global $wgExtensionCredits;
+		foreach ( $wgExtensionCredits['parserhook'] as $ext ) {
+			if ( $ext['name']==='RenderTheFuck' ) {
+				self::$version = $ext['version'];
+				break;
+			}
+		}
 
-        return true;
-    }
+		// Setup callback for <thefuck> tag.
+		$parser->setHook( 'thefuck', [ 'RenderTheFuck', 'render' ] );
 
-    /**
-     * Render <thefuck> tag
-     *
-     * @since 0.1.0
-     *
-     * @param $in     text in <thefuck> tag
-     * @param $param  attributes in <thefuck> tag
-     * @param $parser MediaWiki parser
-     * @param $frame  never used
-     */
-    public static function render($in, $param = array(), $parser = null, $frame = false)
-    {
-        if (self::$rank === 0) {
-            $parser->getOutput()->addModules(array('ext.d3.core'));
-        }
+		return true;
+	}
 
-        $BASE = dirname(__FILE__);
-        $AVAILABLE_EXAMPLES = array('timeline', 'malicious');
+	/**
+	 * Render <thefuck> tag
+	 *
+	 * @since 0.1.0
+	 *
+	 * @param $in     text in <thefuck> tag
+	 * @param $param  attributes in <thefuck> tag
+	 * @param $parser MediaWiki parser
+	 * @param $frame  never used
+	 */
+	public static function render( $in, $param = [], $parser = null, $frame = false ) {
 
-        // Check input for security concern.
-        $svgw = self::getIntValue($param, 'width', 400);
-        $svgh = self::getIntValue($param, 'height', 300);
-        $elid = sprintf('thefuck-%02d', self::$rank++);
+		if ( self::$rank === 0 ) {
+			$parser->getOutput()->addModules( [ 'ext.d3.core' ] );
+		}
 
-        // Decode and encode the json string to ensure it's well-formated.
-        if (isset($param['example']) && in_array($param['example'], $AVAILABLE_EXAMPLES)) {
-            $file = sprintf('%s/examples/%s.tfj', $BASE, $param['example']);
-            $exampleCode = file_get_contents($file);
-            $json = json_encode(json_decode($exampleCode));
-        } else {
-            $json = json_encode(json_decode($in));
-        }
+		$BASE = __DIR__;
+		$AVAILABLE_EXAMPLES = [ 'timeline', 'malicious' ];
 
-        // Server-side validation.
-        if ($json[0] !== '{') {
-            $json = json_encode(array(
-                'wtf' => 'error',
-                'settings' => array(
-                    'message' => 'The fuck is not in correct JSON format.'
-                )
-            ));
-        }
+		// Check input for security concern.
+		$svgw = self::getIntValue( $param, 'width', 400 );
+		$svgh = self::getIntValue( $param, 'height', 300 );
+		$elid = sprintf( 'thefuck-%02d', self::$rank++ );
 
-        // %s id
-        // %d width
-        // %d height
-        // %s json
-        $template = file_get_contents($BASE . '/template.html');
-        $output   = sprintf($template, $elid, $svgw, $svgh, $json);
+		// Decode and encode the json string to ensure it's well-formated.
+		if ( isset( $param['example'] ) && in_array( $param['example'], $AVAILABLE_EXAMPLES ) ) {
+			$file = sprintf( '%s/examples/%s.tfj', $BASE, $param['example'] );
+			$exampleCode = file_get_contents( $file );
+			$json = json_encode( json_decode( $exampleCode ) );
+		} else {
+			$json = json_encode( json_decode( $in ) );
+		}
 
-        // Dump source code of example
-        if (isset($exampleCode) && strlen($exampleCode)>0) {
-            $output .= '<p>Source code of timeline example:</p>';
-            $output .= '<div style="width:90%; height:250px; overflow-y: scroll; border: 1px solid #a7d7f9; border-radius: 5px;">';
-            $output .= '<pre style="margin: 10px;">' . $exampleCode . '</pre>';
-            $output .= '</div>';
-        }
+		// Server-side validation.
+		if ( $json[0] !== '{' ) {
+			$json = json_encode( [
+				'wtf' => 'error',
+				'settings' => [
+					'message' => 'The fuck is not in correct JSON format.'
+				]
+			] );
+		}
 
-        return array( $output, "markerType" => 'nowiki' );
-    }
+		// %s id
+		// %d width
+		// %d height
+		// %s json
+		$template = file_get_contents( $BASE . '/template.html' );
+		$output   = sprintf( $template, $elid, $svgw, $svgh, $json );
 
-    /**
-     * Get given integer or minimal value from key/value array.
-     */
-    private static function getIntValue(&$pairs, $key, $min)
-    {
-        if (isset($pairs[$key])) {
-            $given = (int)$pairs[$key];
-            if ($given > $min) {
-                return $given;
-            }
-        }
-        return $min;
-    }
+		// Dump source code of example
+		if ( isset( $exampleCode ) && strlen( $exampleCode )>0 ) {
+			// TODO: Move styles to resource file
+			$styles = 'width:90%; height:250px; overflow-y: scroll; ' .
+				'border: 1px solid #a7d7f9; border-radius: 5px;';
+
+			$output .= '<p>Source code of timeline example:</p>';
+			$output .= '<div style="' . $styles . '">';
+			$output .= '<pre style="margin: 10px;">' . $exampleCode . '</pre>';
+			$output .= '</div>';
+		}
+
+		return [ $output, "markerType" => 'nowiki' ];
+	}
+
+	/**
+	 * Get given integer or minimal value from key/value array.
+	 */
+	private static function getIntValue( &$pairs, $key, $min ) {
+
+		if ( isset( $pairs[$key] ) ) {
+			$given = (int)$pairs[$key];
+			if ( $given > $min ) {
+				return $given;
+			}
+		}
+		return $min;
+	}
 }
--- modules/genils.php
+++ modules/genils.php
@@ -2,15 +2,14 @@
 /**
  * Generate inline style into javascript variable.
  */
-$items = ['timeline'];
+$items = [ 'timeline' ];
 
-foreach ($items as $i) {
-	$file = sprintf('%s.min.css', $i);
-	$body = file_get_contents($file);
-	$body = str_replace('.render-the-fuck>', '', $body);
-	$jsv  = sprintf('THE_FUCK_STYLES["%s"]="%s";', $i, $body);
+foreach ( $items as $i ) {
+	$file = sprintf( '%s.min.css', $i );
+	$body = file_get_contents( $file );
+	$body = str_replace( '.render-the-fuck>', '', $body );
+	$jsv  = sprintf( 'THE_FUCK_STYLES["%s"]="%s";', $i, $body );
 
-	$file = sprintf('styles.%s.js', $i);
-	file_put_contents($file, $jsv);
+	$file = sprintf( 'styles.%s.js', $i );
+	file_put_contents( $file, $jsv );
 }
-?>
